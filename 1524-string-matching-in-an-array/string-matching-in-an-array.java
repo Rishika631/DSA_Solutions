8f51e678e58c1aa03a1d7ce416ea2d717ee45bc3
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2025-01-07 23:27:05 +0530

    Time: 21 ms (6.83%) | Memory: 45.5 MB (6.00%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -30,3 +30,16 @@ class Solution {
         return ans;
     }
 }
+//Best approach
+// class Solution {
+//     public List<String> stringMatching(String[] words) {
+//         String str = String.join(" ", words);
+//         List<String> res = new ArrayList<>();
+//         for(int i = 0; i < words.length; i++){
+//             if(str.indexOf(words[i]) != str.lastIndexOf(words[i])){
+//                 res.add(words[i]);
+//             }
+//         }
+//         return res;
+//     }
+// }
