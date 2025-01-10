a05eff7f42aa8435946c1ddd310f06f0a6ccb4b8
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2025-01-11 00:08:29 +0530

    Time: 15 ms (30.23%) | Memory: 54.7 MB (74.42%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -38,34 +38,54 @@
         
 //     }
 // }
-
+// for this approach we just take from all the substring words how many times a char is max called like in one substring how many times a single char is used and is that the max count of that char for one substring
+// [ee, eeeo]
+//so for this it does not matter in which substring e is there not we just need to know that the max amount of times e is occuring for a substring from all substring is equal to or grester than the no. of times e is occuring in the words1 string.
+//TC=n2---- words2.length(26+m(max length string of words2))+words1.length(26+m(max length string of words1))
+//SC=n
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        int[] req = new int[26];
-        for (String word : words2) {
-            int[] cur = new int[26];
-            for (char c : word.toCharArray()) {
-                cur[c - 'a']++;
-                req[c - 'a'] = Math.max(req[c - 'a'], cur[c - 'a']);
+        boolean flag=true;
+        List<String> ans=new ArrayList<String>();
+        int[] req=new int[26];
+        for(String substring: words2)
+        {
+            int[] curr=new int[26];
+            char[] arr=substring.toCharArray();
+            for(char ch:arr)
+            {
+                curr[ch-'a']++;
             }
+            for(int i=0;i<26;i++)
+            {
+                req[i]=Math.max(curr[i],req[i]);
+            }
+
         }
-        
-        List<String> ans = new ArrayList<>();
-        for (String word : words1) {
-            int[] cur = new int[26];
-            for (char c : word.toCharArray()) cur[c - 'a']++;
-            
-            boolean isValid = true;
-            for (int i = 0; i < 26; i++) {
-                if (cur[i] < req[i]) {
-                    isValid = false;
+
+        for(String words: words1)
+        {
+            int[] curr=new int[26];
+            char[] arr=words.toCharArray();
+            for(char ch:arr)
+            {
+                curr[ch-'a']++;
+            }
+            for(int i=0;i<26;i++)
+            {
+                if(curr[i]<req[i])
+                {
+                    flag=false;
                     break;
                 }
             }
-            
-            if (isValid) ans.add(word);
+            if(flag)
+            {
+                ans.add(words);
+            }
+            flag=true;
+
         }
-        
         return ans;
     }
 }
