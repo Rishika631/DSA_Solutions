024c3323ecda4133c954225c41cbcf71501fbbcf
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2025-02-02 19:35:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (80.04%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,55 @@
 //TC=3n----O(n)
 //SC=n as array
+//My approach correct but more space
 
+// class Solution {
+//     public boolean check(int[] nums) {
+//         int[] b=new int[nums.length];
+//         int x=0;
+//         for(int i=0;i<nums.length-1;i++)
+//         {
+//             if(nums[i+1]<nums[i])
+//             {
+//                 x=i+1;
+//                 break;
+//             }
+//         }
+//         for(int j=0;j<nums.length;j++)
+//         {
+//            b[j]=nums[(j+x)%nums.length];
+//         }
+//         for(int j=0;j<b.length-1;j++)
+//         {
+//            if(b[j]>b[j+1])
+//            {
+//             return false;
+//            }
+//         }
+//         return true;
+//     }
+// }
+
+//editorial approach
 class Solution {
+
     public boolean check(int[] nums) {
-        int[] b=new int[nums.length];
-        int x=0;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            if(nums[i+1]<nums[i])
-            {
-                x=i+1;
-                break;
+        int n = nums.length;
+        if (n <= 1) return true;
+
+        int inversionCount = 0;
+
+        // For every pair, count the number of inversions.
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                ++inversionCount;
             }
         }
-        for(int j=0;j<nums.length;j++)
-        {
-           b[j]=nums[(j+x)%nums.length];
+
+        // Also check between the last and the first element due to rotation
+        if (nums[0] < nums[n - 1]) {
+            ++inversionCount;
         }
-        for(int j=0;j<b.length-1;j++)
-        {
-           if(b[j]>b[j+1])
-           {
-            return false;
-           }
-        }
-        return true;
+
+        return inversionCount <= 1;//< for all ele same num in array like[1,1,1,1]
     }
 }
