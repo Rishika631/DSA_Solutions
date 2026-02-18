01a335f5a55672ca9bb7faecca57c700fda4c793
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-02-18 19:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (19.35%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,6 +1,6 @@
 // if not 9 then add one and return if 9 then make it zero and reduce i as it will go to next num and if not return in while loop means [9,9,9] case so add 1 at beginning of array and return
 // time=n(loop)+n(insert)
-//space=const
+//space=2n new array + array list
 class Solution {
     public int[] plusOne(int[] digits) {
         int i=digits.length-1;
@@ -28,3 +28,22 @@ class Solution {
         return res;
     }
 }
+
+//approach where no array list as when initialize new array it is 0 by default 
+//time=n
+//space n as no array list issue
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+//         for (int i = digits.length - 1; i >= 0; i--) {
+//             if (digits[i] + 1 != 10) {
+//                 digits[i] += 1;
+//                 return digits;
+//             }
+//             digits[i] = 0;
+//         }
+        
+//         int[] newDigits = new int[digits.length + 1];
+//         newDigits[0] = 1;
+//         return newDigits;        
+//     }
+// }
