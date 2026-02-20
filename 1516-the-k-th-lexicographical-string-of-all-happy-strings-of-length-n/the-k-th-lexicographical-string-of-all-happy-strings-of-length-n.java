8c14ec23c2dc1cc50d102bfcd6d6d12dca6b4bdb
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-02-20 19:31:07 +0530

    Time: 12 ms (38.97%) | Memory: 46.4 MB (42.06%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,23 +1,36 @@
-import java.util.*;
+
 
 class Solution {
-    List<String> happy = new ArrayList<>();
-    
-    public String getHappyString(int n, int k) {
-        char[] chars = {'a', 'b', 'c'};
-        generateHappyString("", n, chars);
-        return k > happy.size() ? "" : happy.get(k - 1);
-    }
 
-    private void generateHappyString(String path, int n, char[] chars) {
-        if (path.length() == n) {
-            happy.add(path);
+    public void solve(int n, int k, List<String> result, StringBuilder curr){
+        if(curr.length()==n)
+        {
+            result.add(curr.toString());
             return;
         }
-        for (char c : chars) {
-            if (path.isEmpty() || path.charAt(path.length() - 1) != c) {
-                generateHappyString(path + c, n, chars);
+        for(char ch='a';ch<='c';ch++)
+        {
+            if(curr.length()!=0 && curr.charAt(curr.length()-1)==ch)
+            {
+                continue;
             }
+            curr.append(ch);
+            solve(n,k,result,curr);
+            curr.deleteCharAt(curr.length()-1);
+
         }
+    }
+
+
+    public String getHappyString(int n, int k) {
+        
+        StringBuilder curr=new StringBuilder();
+        List<String> result=new ArrayList<>();
+        solve(n, k, result, curr);
+        if(result.size()<k)
+        {
+            return "";
+        }
+        return result.get(k-1);
     }
 }
