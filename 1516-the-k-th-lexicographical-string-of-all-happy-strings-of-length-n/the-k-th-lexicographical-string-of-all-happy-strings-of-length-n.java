e25f063dd9add9d52aaaf955fd885c26d4798a7f
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-02-20 19:51:20 +0530

    Time: 8 ms (44.85%) | Memory: 42.7 MB (98.68%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,11 +1,56 @@
 
+// //Brute force
+// // time= n(for loop)* 3*2^n-1(for possible strings)
+// //space=n(length of string)*3*2^n-1(possible strings)
+// class Solution {
 
+//     public void solve(int n, int k, List<String> result, StringBuilder curr){
+//         if(curr.length()==n)
+//         {
+//             result.add(curr.toString());
+//             return;
+//         }
+//         for(char ch='a';ch<='c';ch++)
+//         {
+//             if(curr.length()!=0 && curr.charAt(curr.length()-1)==ch)
+//             {
+//                 continue;
+//             }
+//             curr.append(ch);
+//             solve(n,k,result,curr);
+//             curr.deleteCharAt(curr.length()-1);
+
+//         }
+//     }
+
+
+//     public String getHappyString(int n, int k) {
+        
+//         StringBuilder curr=new StringBuilder();
+//         List<String> result=new ArrayList<>();
+//         solve(n, k, result, curr);
+//         if(result.size()<k)
+//         {
+//             return "";
+//         }
+//         return result.get(k-1);
+//     }
+// }
+
+
+//optimized
+// time= n(for loop)* 3*2^n-1(for possible strings)
+//space=n(length of string)- for recurssion tree not storing the result now
 class Solution {
 
-    public void solve(int n, int k, List<String> result, StringBuilder curr){
+    public void solve(int n, int k, int[] counter, StringBuilder curr, String[] result){
         if(curr.length()==n)
         {
-            result.add(curr.toString());
+            counter[0]+=1;
+            if(counter[0]==k)
+            {
+                result[0]=curr.toString();
+            }
             return;
         }
         for(char ch='a';ch<='c';ch++)
@@ -15,7 +60,7 @@ class Solution {
                 continue;
             }
             curr.append(ch);
-            solve(n,k,result,curr);
+            solve(n,k,counter,curr, result);
             curr.deleteCharAt(curr.length()-1);
 
         }
@@ -25,12 +70,9 @@ class Solution {
     public String getHappyString(int n, int k) {
         
         StringBuilder curr=new StringBuilder();
-        List<String> result=new ArrayList<>();
-        solve(n, k, result, curr);
-        if(result.size()<k)
-        {
-            return "";
-        }
-        return result.get(k-1);
+        int[] counter={0};
+        String[] result={""};
+        solve(n, k, counter, curr, result);
+        return result[0];
     }
 }
