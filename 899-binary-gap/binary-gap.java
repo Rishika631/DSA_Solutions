c5e757caa2f0df877e221638bee105e25adbada7
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-02-23 00:08:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (67.94%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -3,44 +3,44 @@
 //n-10^9 which is 2^30 means 30 bits2^10- aprrox 103 cubes both sides and get above so max 30 bits
 //space-const as binary string const space 32 bits
 
-class Solution {
-    public int binaryGap(int n) {
-        String str=Integer.toBinaryString(n);//logn + 1 time complexity as divide by 2 so num of bits// it gives no leading zeroes
-        int len=str.length();
-        int result=0;// best to keep 0 as int.min val will fail for cases 000 but we donot have that in input isliye it passes with int min also
-        int i=0;
-        int start=0;//only works if not leading zeroes
-        while(i<len)
-        {
-            if(str.charAt(i)=='1')
-            {
-                result=Math.max(result,i-start);
-                start=i;
-            }
-            i++;
-        }
-        return result;
-    }
-}
-
-//bitwise methods
-// for both time - const as 30 bits only and space const as nothing new we take
 // class Solution {
 //     public int binaryGap(int n) {
-        
-//         int result=0;
-//         int curr=0;
-//         int prev=-1;
-//         while(n>0)
+//         String str=Integer.toBinaryString(n);//logn + 1 time complexity as divide by 2 so num of bits// it gives no leading zeroes
+//         int len=str.length();
+//         int result=0;// best to keep 0 as int.min val will fail for cases 000 but we donot have that in input isliye it passes with int min also
+//         int i=0;
+//         int start=0;//only works if not leading zeroes
+//         while(i<len)
 //         {
-//             if((n&1)>0)//to check if the rightmost bit is set or not do n&1
+//             if(str.charAt(i)=='1')
 //             {
-//                 result=prev != -1?Math.max(result,curr-prev):result;
-//                 prev=curr;
+//                 result=Math.max(result,i-start);
+//                 start=i;
 //             }
-//             curr++;
-//             n>>=1;// to right shift num by 1
+//             i++;
 //         }
 //         return result;
 //     }
 // }
+
+//bitwise methods
+// for both time - const as 30 bits only and space const as nothing new we take
+class Solution {
+    public int binaryGap(int n) {
+        
+        int result=0;
+        int curr=0;
+        int prev=-1;
+        while(n>0)
+        {
+            if((n&1)>0)//to check if the rightmost bit is set or not do n&1
+            {
+                result=prev != -1?Math.max(result,curr-prev):result;
+                prev=curr;
+            }
+            curr++;
+            n>>=1;// to right shift num by 1
+        }
+        return result;
+    }
+}
