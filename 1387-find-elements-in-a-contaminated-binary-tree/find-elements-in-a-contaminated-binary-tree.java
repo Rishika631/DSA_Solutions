9e30012aae6487c3f9e0512beebe5d8ee8718b73
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-02-22 18:17:45 +0530

    Time: 25 ms (28.57%) | Memory: 49.9 MB (10.61%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
@@ -13,53 +13,80 @@
  *     }
  * }
  */
-import java.util.*;
+ //dfs-recursion
+ //time-n- for dfs goes to every node
+ //space-o(n)-hashset
+// class FindElements {
+//     HashSet<Integer> hs=new HashSet<>();
 
-class FindElements {
-    private List<Integer> valuesList = new ArrayList<>();
+//     public void dfs(TreeNode root, int num, HashSet<Integer> hs)
+//     {
+//         if(root==null)
+//         {
+//             return;
+//         }
+//         root.val=num;
+//         hs.add(num);
+//         dfs(root.left, 2*num+1, hs);
+//         dfs(root.right, 2*num+2, hs);
+//     }
 
-    /**
-     * Recursively restores the values of the tree nodes.
-     */
-    private void solve(TreeNode root) {
-        if (root == null) {
-            return; // Base case: If node is null, stop recursion
-        }
+//     public FindElements(TreeNode root) {
 
-        // Store the restored value
-        valuesList.add(root.val);
+//         hs.clear();
+//         dfs(root, 0, hs);
 
-        // Process left child
-        if (root.left != null) {
-            root.left.val = root.val * 2 + 1;
-        }
-        solve(root.left);
-
-        // Process right child
-        if (root.right != null) {
-            root.right.val = root.val * 2 + 2;
-        }
-        solve(root.right);
-    }
-
-    /**
-     * Constructor initializes the recovery process of the tree.
-     */
-    public FindElements(TreeNode root) {
-        root.val = 0;
-        solve(root);
-    }
-
-    /**
-     * Checks if a given `target` value exists in the recovered tree.
-     */
-    public boolean find(int target) {
-        return valuesList.contains(target);
-    }
-}
+//     }
+    
+//     public boolean find(int target) {
+//         return hs.contains(target);
+//     }
+// }
 
 /**
  * Your FindElements object will be instantiated and called as such:
  * FindElements obj = new FindElements(root);
  * boolean param_1 = obj.find(target);
  */
+
+//bfs
+//space-2n (one for queue and one for hs)
+//time-n(all nodes)
+ class FindElements {
+    HashSet<Integer> hs=new HashSet<>();
+
+    public void bfs(TreeNode root, int num, HashSet<Integer> hs)
+    {
+        Queue<TreeNode> que=new LinkedList<>();//queue so root then left then right then left ka left n all
+        root.val=num;
+        que.add(root);
+        while(!que.isEmpty())
+        {
+            TreeNode temp=que.remove();
+            int val=temp.val;
+            hs.add(val);
+
+            if(temp.left !=null)
+            {
+                temp.left.val=2*val+1;
+                que.add(temp.left);
+            }
+            if(temp.right !=null)
+            {
+                temp.right.val=2*val+2;
+                que.add(temp.right);
+            }
+        }
+    }
+
+    public FindElements(TreeNode root) {
+
+        hs.clear();
+        bfs(root, 0, hs);
+
+    }
+    
+    public boolean find(int target) {
+        return hs.contains(target);
+    }
+}
