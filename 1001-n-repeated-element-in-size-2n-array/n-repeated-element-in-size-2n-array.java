a4fc0f8f2fc1b2cbc27d4e14dc2bbf893205ce52
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-03-01 23:55:09 +0530

    Time: 1 ms (50.12%) | Memory: 47.2 MB (88.46%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -28,23 +28,44 @@
 
 // space-n
 //time-n
+// class Solution {
+//     public int repeatedNTimes(int[] nums) {
+//         int n=nums.length/2;
+//         int ans=0;
+//         HashSet<Integer> hs=new HashSet<>();
+//         for(int i=0;i<=n+1;i++)// as n+1 unique values so need 1 extra so any ele comes twice
+//         {
+//             if(hs.contains(nums[i]))
+//             {
+//                 ans=nums[i];
+//                 break;
+//             }
+//             else
+//             {
+//                 hs.add(nums[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// space-const space approach as 10^4 const
+//time-n
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        int n=nums.length/2;
-        int ans=0;
-        HashSet<Integer> hs=new HashSet<>();
-        for(int i=0;i<=n+1;i++)// as n+1 unique values so need 1 extra so any ele comes twice
+        int[] arr=new int[10000];
+        for(int i=0;i<nums.length;i++)
         {
-            if(hs.contains(nums[i]))
+            if(arr[nums[i]]==1)
             {
-                ans=nums[i];
-                break;
+                
+                return nums[i];
             }
             else
             {
-                hs.add(nums[i]);
+                arr[nums[i]]=1;
             }
         }
-        return ans;
+        return -1;// never reach this 
     }
 }
