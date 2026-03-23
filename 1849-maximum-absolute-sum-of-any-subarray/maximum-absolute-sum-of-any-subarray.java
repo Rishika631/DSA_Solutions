23a5accf6940ad1dd27eace8351b888794fe3aea
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-03-23 17:47:28 +0530

    Time: 7 ms (57.19%) | Memory: 66.9 MB (42.22%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -1,20 +1,30 @@
+// brute force have two for loops and check all possible value and give max sum
+// Time - o(n^2)
+//Space- const
+
+// kandane's algo
+//time- o(n) one for loop
+// space- const
 class Solution {
     public int maxAbsoluteSum(int[] nums) {
-        int n = nums.length;
-        int maxEndingHere = nums[0];
-        int minEndingHere = nums[0];
-        int maxSoFar = nums[0];
-        int minSoFar = nums[0];
         
-        // Kadane's algorithm to find max and min subarray sums
-        for (int i = 1; i < n; i++) {
-            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
-            minEndingHere = Math.min(nums[i], minEndingHere + nums[i]);
-            maxSoFar = Math.max(maxSoFar, maxEndingHere);
-            minSoFar = Math.min(minSoFar, minEndingHere);
+        // absolute donot ignore negative
+        // abosolute so have min and max subarray sum as abs karne pr -ve can be big also
+
+        int len=nums.length;
+        int maxSubarraySum=nums[0];
+        int currMaxSubarraySum=nums[0];
+        int minSubarraySum=nums[0];
+        int currMinSubarraySum=nums[0];
+
+        for(int i=1;i<len;i++)
+        {
+            currMaxSubarraySum= Math.max(nums[i], currMaxSubarraySum+nums[i]);
+            maxSubarraySum=Math.max(maxSubarraySum, currMaxSubarraySum);
+            currMinSubarraySum= Math.min(nums[i], currMinSubarraySum+nums[i]);
+            minSubarraySum=Math.min(minSubarraySum, currMinSubarraySum);
         }
-        
-        // The maximum absolute sum is the maximum of the absolute values of maxSoFar and minSoFar
-        return Math.max(Math.abs(maxSoFar), Math.abs(minSoFar));
+        return Math.max(Math.abs(maxSubarraySum), Math.abs(minSubarraySum));
+
     }
 }
