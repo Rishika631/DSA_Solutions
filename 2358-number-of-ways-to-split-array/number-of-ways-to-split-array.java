d7d0ccc552458fec7fe5ebcc0a0dfbd409a04bce
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-03-28 20:30:47 +0530

    Time: 2 ms (100.00%) | Memory: 102.2 MB (48.33%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,20 +1,52 @@
+
+//pure brute force have a for loop for split and then inside that two for loops one for right and one for left for sum
+//time-n^2
+//space-const
+//below prefix sum approach
 // time- o(2n)
 //space-o(n)
 
+// class Solution {
+//     public int waysToSplitArray(int[] nums) {
+//         int len=nums.length;
+//         int result=0;
+//         long[] prefix= new long[len];// as very large value when added 
+//         prefix[0]=nums[0];
+//         for(int i=1;i<len;i++)
+//         {
+//             prefix[i]=prefix[i-1]+nums[i];
+//         }
+//         for(int i=0;i<len-1;i++)//len-1 as we donot want right to be empty 2nd condition in ques
+//         {
+//             long right=prefix[len-1]-prefix[i];
+//             if(prefix[i]>=right)//left>=right
+//             {
+//                 result++;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+//more optimized
+// time-o(2n)
+//space- const we donot need prefix array astotal mai say left wale ko minus kar rhe can do this way as well
 class Solution {
     public int waysToSplitArray(int[] nums) {
         int len=nums.length;
         int result=0;
-        long[] prefix= new long[len];// as very large value when added 
-        prefix[0]=nums[0];
-        for(int i=1;i<len;i++)
+        long sum=0;
+        for(int i=0;i<len;i++)
         {
-            prefix[i]=prefix[i-1]+nums[i];
+            sum+=nums[i];
         }
+        long leftsum=0;
+        long rightsum=0;
         for(int i=0;i<len-1;i++)//len-1 as we donot want right to be empty 2nd condition in ques
         {
-            long right=prefix[len-1]-prefix[i];
-            if(prefix[i]>=right)//left>=right
+            leftsum+=nums[i];
+            rightsum=sum-leftsum;
+            if(leftsum>=rightsum)//left>=right
             {
                 result++;
             }
