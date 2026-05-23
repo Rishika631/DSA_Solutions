869aadad9698aab2dbc930a98166a47d54c03a5e
Rishika Agrawal <89201634+Rishika631@users.noreply.github.com>
2026-05-23 23:44:50 +0530

    Time: 5 ms (1.95%) | Memory: 43.1 MB (44.94%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,56 +1,28 @@
-//TC=3n----O(n)
-//SC=n as array
-//My approach correct but more space
-
-// class Solution {
-//     public boolean check(int[] nums) {
-//         int[] b=new int[nums.length];
-//         int x=0;
-//         for(int i=0;i<nums.length-1;i++)
-//         {
-//             if(nums[i+1]<nums[i])
-//             {
-//                 x=i+1;
-//                 break;
-//             }
-//         }
-//         for(int j=0;j<nums.length;j++)
-//         {
-//            b[j]=nums[(j+x)%nums.length];
-//         }
-//         for(int j=0;j<b.length-1;j++)
-//         {
-//            if(b[j]>b[j+1])
-//            {
-//             return false;
-//            }
-//         }
-//         return true;
-//     }
-// }
-
-//editorial approach
-//better appraoch as sc is o(1) means const with tc=o(n)
 class Solution {
 
     public boolean check(int[] nums) {
-        int n = nums.length;
-        if (n <= 1) return true;
+        int size = nums.length;
 
-        int inversionCount = 0;
+        // Create a sorted copy of the array
+        int[] sortedNums = nums.clone();
+        Arrays.sort(sortedNums);
 
-        // For every pair, count the number of inversions.
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] < nums[i - 1]) {
-                ++inversionCount;
+        // Compare the original array with the sorted array, considering all possible rotations
+        for (int rotationOffset = 0; rotationOffset < size; ++rotationOffset) {
+            boolean isMatch = true;
+            for (int index = 0; index < size; ++index) {
+                if (
+                    nums[(rotationOffset + index) % size] != sortedNums[index]
+                ) {
+                    isMatch = false;
+                    break;
+                }
+            }
+            if (isMatch) {
+                return true;
             }
         }
 
-        // Also check between the last and the first element due to rotation
-        if (nums[0] < nums[n - 1]) {
-            ++inversionCount;
-        }
-
-        return inversionCount <= 1;//< for all ele same num in array like[1,1,1,1]
+        return false;
     }
 }
